ed8cf01a3239dbb3ee3890641f1d23fa054b3a44
Jonathan Santerre <santerrejonathan@gmail.com>
2014-03-03 19:34:23 -0500

    il manquait une parenthese dans le if statement

--- src/org/usfirst/frc3360/Hyperion2014/commands/CanonSpinner_ReceivePass.java
+++ src/org/usfirst/frc3360/Hyperion2014/commands/CanonSpinner_ReceivePass.java
@@ -35,7 +35,7 @@ public class  CanonSpinner_ReceivePass extends Command {
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        if(!m_bIsInit {
+        if(!m_bIsInit) {
             Robot.canonSpinner.InitCatchSpeed();
             
             if(isTimedOut()){
