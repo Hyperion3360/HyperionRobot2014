6a2da5b5630b6aca266eaef33fe46ca3e57454b9
Alexandre Malo <malo.lexandre@gmail.com>
2014-03-07 12:57:13 -0500

    mode autonome a 1 ballon pu de bug

--- src/org/usfirst/frc3360/Hyperion2014/commands/AutonomousMode.java
+++ src/org/usfirst/frc3360/Hyperion2014/commands/AutonomousMode.java
@@ -19,7 +19,14 @@ public class AutonomousMode extends CommandGroup {
     
     public  AutonomousMode() {
         addParallel(new DriveTrain_Idle(0));
-        addSequential(new Canon_ShootTopGoal());
+       // addSequential(new Canon_ShootTopGoal());
+        
+        addParallel(new CanonSpinner_PrepareTopGoal());
+        addParallel(new CanonAngle_SetShooterAngle(45.6));
+        addSequential(new SystemWait(2));
+        addParallel(new CanonShooter_Shoot());
+        addSequential(new SystemWait(1));
+        
         addSequential(new DriveTrain_MoveTo(2));
         addParallel(new DriveTrain_Idle(0));
         addSequential(new CanonSpinner_Brake());
