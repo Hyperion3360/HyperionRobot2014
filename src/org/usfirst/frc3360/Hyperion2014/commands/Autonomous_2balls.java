3b47d986c8aa314238c02fd20d8645c1f31dd8df
Alexandre Malo <malo.lexandre@gmail.com>
2014-03-08 12:20:55 -0500

    Tuned the two ball autonomous mode.

--- src/org/usfirst/frc3360/Hyperion2014/commands/Autonomous_2balls.java
+++ src/org/usfirst/frc3360/Hyperion2014/commands/Autonomous_2balls.java
@@ -17,22 +17,21 @@ public class Autonomous_2balls extends CommandGroup{
         // Shoot first ball, and wait a few to make sure one on two
         // in the hot goal (hence the wait).
         addParallel(new CanonSpinner_PrepareTopGoal());
-        addParallel(new CanonAngle_SetShooterAngle(45.6));
+        addParallel(new CanonAngle_SetShooterAngle(48));
         addSequential(new SystemWait(2));
-        addParallel(new CanonShooter_Shoot());
-        addSequential(new SystemWait(1));
+        addSequential(new CanonShooter_Shoot());
         
         // Prepare to grab the ball in front.
         addSequential(new Canon_CopilotGrab(false, true), 1);
         
         // Move forward to eat the ball and wait a bit
         // to make sure the ball is in.        
-        addSequential(new DriveTrain_MoveTo(2.5));
+        addSequential(new DriveTrain_MoveTo(4));
         
         // Shoot the second ball
         addParallel(new DriveTrain_Idle(0));
         // Prepare the able and let the ball the time to enter.
-        addSequential(new CanonAngle_SetShooterAngle(55), 1);
+        addSequential(new CanonAngle_SetShooterAngle(55), 1.75);
         addSequential(new Canon_ShootTopGoal());
         addSequential(new DriveTrain_MoveTo(1.5));
         
